30a59c0e0244d02b87364c4652f3fa9bd92b8293
qianlx <qianlx@boloomo.com>
2019-04-28 15:59:06 +0800

    init: 初始化人事管理项目（spring mvc）

---
+++ src/org/java/hrm/domain/User.java
@@ -0,0 +1,65 @@
+package org.java.hrm.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class User implements Serializable {
+    private Integer id;
+    private String username;
+    private String loginname;
+    private String password;
+    private Integer userstatus;
+    private Date createdate;
+
+    public User() {
+        super();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getLoginname() {
+        return loginname;
+    }
+
+    public void setLoginname(String loginname) {
+        this.loginname = loginname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getUserstatus() {
+        return userstatus;
+    }
+
+    public void setUserstatus(Integer userstatus) {
+        this.userstatus = userstatus;
+    }
+
+    public Date getCreatedate() {
+        return createdate;
+    }
+
+    public void setCreatedate(Date createdate) {
+        this.createdate = createdate;
+    }
+}
